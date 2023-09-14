bf27c63e736cab0c32e595bad2fffa85cfb438b2
Ankit Dash <ankitdash2019@gmail.com>
2023-09-14 22:01:43 +0530

    started with implementation of policy network

---
+++ src/PolicyNetwork.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class PolicyNetwork:
+    def __init__(self, input_size, output_size):
+        super(PolicyNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, 64)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(64, output_size)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return self.softmax(x)
